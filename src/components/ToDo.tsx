5ee90007041b703ff9e266ee848db892634a94ff
Wholesome-Gee <jiyong0419@naver.com>
2025-04-24 14:22:52 +0900

    codeChallenge 1
    삭제버튼 만들기

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Categories, IToDo, toDoState } from "../atom";
-import { useSetRecoilState } from "recoil";
+import { useRecoilState, useSetRecoilState } from "recoil";
 
 function ToDo({ text, category, id }: IToDo) {
-  const setToDos = useSetRecoilState(toDoState);
+  const [toDos, setToDos] = useRecoilState(toDoState);
 
   function changeCategory(event: React.MouseEvent<HTMLButtonElement>) {
     setToDos((toDos) => {
@@ -14,6 +14,20 @@ function ToDo({ text, category, id }: IToDo) {
 
       return [...toDos.slice(0, targetIndex), newToDo, ...toDos.slice(targetIndex + 1)];
       // Array.slice(index1,index2) = 배열의 index1부터 index2까지 있는 요소들을 제거 후 새로운 배열을 반환한다.  #6.15
+    });
+  }
+
+  // codeChallenge
+  function deleteToDo(event: React.MouseEvent<HTMLButtonElement>) {
+    /*
+    ToDos배열에서 해당 toDo를 삭제한다.
+    1. toDos를 불러온다.
+    2. toDos에서 해당 toDo를 찾는다.
+    3. splice를 이용해 제거한다.
+    */
+    setToDos((toDos) => {
+      const targetIndex = toDos.findIndex((toDo) => toDo.id === id);
+      return [...toDos.slice(0, targetIndex), ...toDos.slice(targetIndex + 1)];
     });
   }
 
@@ -35,6 +49,7 @@ function ToDo({ text, category, id }: IToDo) {
           DONE
         </button>
       )}
+      <button onClick={deleteToDo}>삭제</button>
     </li>
   );
 }
